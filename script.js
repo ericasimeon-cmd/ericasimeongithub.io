340685aceefbe72b19641590ecbf9564759be964
ericasimeon-cmd <erica.simeon@csucc.edu.ph>
2025-11-30 14:58:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+        function login() {
+            document.getElementById("login-page").style.display = "none";
+            document.getElementById("portfolio-page").style.display = "block";
+        }
+
+        function logout() {
+            document.getElementById("portfolio-page").style.display = "none";
+            document.getElementById("login-page").style.display = "flex";
+        }
+
+    </script>
